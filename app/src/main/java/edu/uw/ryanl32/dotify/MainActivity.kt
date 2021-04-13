43e3b1619f29d8ed6d7063b7d49d10505c758ae3
Ryan Lee <ralee478.school@gmail.com>
2021-04-12 17:00:36 -0700

    finished 1st draft layout, started declaring local vars for logic

--- app/src/main/java/edu/uw/ryanl32/dotify/MainActivity.kt
+++ app/src/main/java/edu/uw/ryanl32/dotify/MainActivity.kt
@@ -2,8 +2,17 @@ package edu.uw.ryanl32.dotify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var numPlays = 0
+    private lateinit var username: EditText
+    private lateinit var btnChangeUser: Button
+    private lateinit var numPlaysDisplay: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

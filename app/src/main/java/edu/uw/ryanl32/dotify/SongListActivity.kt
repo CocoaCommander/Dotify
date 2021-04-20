0f93343c8145cfaaeea1384c6a96c4770d3bfa82
Ryan Lee <ralee478.school@gmail.com>
2021-04-19 23:35:13 -0700

    implemented SongListActivity and SongAdapter

---
+++ app/src/main/java/edu/uw/ryanl32/dotify/SongListActivity.kt
@@ -0,0 +1,34 @@
+package edu.uw.ryanl32.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.ericchee.songdataprovider.SongDataProvider
+import edu.uw.ryanl32.dotify.databinding.ActivitySongListBinding
+
+class SongListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySongListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_song_list)
+
+        binding = ActivitySongListBinding.inflate(layoutInflater).apply { setContentView(root) }
+
+        setTitle("All Songs")
+
+        with(binding) {
+            val allSongs = SongDataProvider.getAllSongs()
+
+            val adapter = SongListAdapter(allSongs)
+
+            rvSongList.adapter = adapter
+
+            adapter.onSongClickListener = { position, song ->
+                Toast.makeText(this@SongListActivity, "Position: $position SongTitle ${song.title}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/edu/uw/ryanl32/dotify/SongListAdapter.kt
@@ -0,0 +1,42 @@
+package edu.uw.ryanl32.dotify
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ericchee.songdataprovider.Song
+import edu.uw.ryanl32.dotify.databinding.ItemSongBinding
+
+class SongListAdapter(private var listOfSongs: List<Song>): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
+
+    var onSongClickListener: (position: Int, song: Song) -> Unit = {_, _ -> }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        val binding = ItemSongBinding.inflate(LayoutInflater.from(parent.context))
+        return SongViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        val song = listOfSongs[position]
+        with(holder.binding) {
+            tvSongListArtistName.text = song.artist
+            tvSongListSongName.text = song.title
+            ivSongPic.setImageResource(song.smallImageID)
+
+            itemRoot.setOnClickListener {
+                onSongClickListener(position, song)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listOfSongs.size
+    }
+
+    fun updateSongs(newListofSongs: List<Song>) {
+        this.listOfSongs = newListofSongs
+
+        notifyDataSetChanged()
+    }
+
+    class SongViewHolder(val binding: ItemSongBinding): RecyclerView.ViewHolder(binding.root)
+}

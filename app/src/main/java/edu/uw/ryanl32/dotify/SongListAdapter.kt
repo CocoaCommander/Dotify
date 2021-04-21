881248e7c03966a1fc7b089e709ca66fe8b4ae88
Ryan Lee <ralee478.school@gmail.com>
2021-04-21 00:15:13 -0700

    Shuffling works-ish, doesn't shuffle the selected song until it goes out of view, app crashes after a few shuffles

--- app/src/main/java/edu/uw/ryanl32/dotify/SongListAdapter.kt
+++ app/src/main/java/edu/uw/ryanl32/dotify/SongListAdapter.kt
@@ -2,13 +2,20 @@ package edu.uw.ryanl32.dotify
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.ericchee.songdataprovider.Song
 import edu.uw.ryanl32.dotify.databinding.ItemSongBinding
 
 class SongListAdapter(private var listOfSongs: List<Song>): RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
 
+    private val mutableSongs = mutableListOf<Song>()
+
     var onSongClickListener: (position: Int, song: Song) -> Unit = {_, _ -> }
+
+    init {
+        mutableSongs.addAll(listOfSongs)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
         val binding = ItemSongBinding.inflate(LayoutInflater.from(parent.context))
@@ -16,7 +23,7 @@ class SongListAdapter(private var listOfSongs: List<Song>): RecyclerView.Adapter
     }
 
     override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
-        val song = listOfSongs[position]
+        val song = mutableSongs[position]
         with(holder.binding) {
             tvSongListArtistName.text = song.artist
             tvSongListSongName.text = song.title
@@ -29,14 +36,33 @@ class SongListAdapter(private var listOfSongs: List<Song>): RecyclerView.Adapter
     }
 
     override fun getItemCount(): Int {
-        return listOfSongs.size
+        return mutableSongs.size
     }
 
     fun updateSongs(newListOfSongs: List<Song>) {
-        this.listOfSongs = newListOfSongs
+        val diffCallback = SongDiffCallback(this.mutableSongs, newListOfSongs)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        this.mutableSongs.clear()
+        this.mutableSongs.addAll(newListOfSongs)
 
-        notifyDataSetChanged()
+        diffResult.dispatchUpdatesTo(this)
     }
 
     class SongViewHolder(val binding: ItemSongBinding): RecyclerView.ViewHolder(binding.root)
+
+    class SongDiffCallback(
+        private val oldList: List<Song>,
+        private val newList: List<Song>
+    ) : DiffUtil.Callback() {
+        override fun getNewListSize(): Int = newList.size
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].smallImageID == newList[newItemPosition].smallImageID
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].title == newList[newItemPosition].title
+        }
+    }
 }

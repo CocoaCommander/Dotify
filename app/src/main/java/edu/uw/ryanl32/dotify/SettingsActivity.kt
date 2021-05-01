8c4573e64157a793b5ce743d9c96350ccd48a822
Ryan Lee <ralee478.school@gmail.com>
2021-05-01 15:17:06 -0700

    created settings activity and associated fragments

---
+++ app/src/main/java/edu/uw/ryanl32/dotify/SettingsActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.ryanl32.dotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}

47648dc360dbc1459d0d5c1035b2287660ea4002
Ryan Lee <ralee478.school@gmail.com>
2021-05-01 18:04:20 -0700

    data is passed between activities and fragments, buttons in fragments route to correct places

--- app/src/main/java/edu/uw/ryanl32/dotify/PlayerActivity.kt
+++ app/src/main/java/edu/uw/ryanl32/dotify/PlayerActivity.kt
@@ -6,24 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
 import com.ericchee.songdataprovider.Song
+import com.ericchee.songdataprovider.SongDataProvider
 import edu.uw.ryanl32.dotify.databinding.ActivityPlayerBinding
 import kotlin.random.Random
 
-private const val SONG_TITLE_KEY = "song_title"
-private const val SONG_ARTIST_KEY = "song_artist"
-private const val SONG_IMAGE_KEY = "song_image"
+//private const val SONG_TITLE_KEY = "song_title"
+//private const val SONG_ARTIST_KEY = "song_artist"
+//private const val SONG_IMAGE_KEY = "song_image"
 private const val SONG_KEY = "song_key"
 
 fun startPlayerActivity(context: Context, song: Song) {
     with(context) {
         val intent = Intent(context, PlayerActivity::class.java).apply {
-            val bundle = Bundle().apply {
-                putString(SONG_TITLE_KEY, song.title)
-                putString(SONG_ARTIST_KEY, song.artist)
-                putInt(SONG_IMAGE_KEY, song.largeImageID)
-                putParcelable(SONG_KEY, song)
-            }
-            putExtras(bundle)
+//            val bundle = Bundle().apply {
+//                putString(SONG_TITLE_KEY, song.title)
+//                putString(SONG_ARTIST_KEY, song.artist)
+//                putInt(SONG_IMAGE_KEY, song.largeImageID)
+//                putParcelable(SONG_KEY, song)
+//            }
+//            putExtras(bundle)
+
+            putExtra(SONG_KEY, song)
         }
         startActivity(intent)
     }
@@ -40,7 +43,7 @@ class PlayerActivity : AppCompatActivity() {
         binding = ActivityPlayerBinding.inflate(layoutInflater).apply { setContentView(root) }
 
         with(binding) {
-            tvNumPlays.text = "${numPlaysCounter.toString()} plays"
+            tvNumPlays.text = "${numPlaysCounter} plays"
 
             btnPrev.setOnClickListener {
                 btnPrevClicked()
@@ -52,15 +55,23 @@ class PlayerActivity : AppCompatActivity() {
                 tvNumPlays.text = btnPlayClicked()
             }
 
-            val image: Int? = intent.extras?.getInt(SONG_IMAGE_KEY, 0)
-            val title: String? = intent.extras?.getString(SONG_TITLE_KEY, "")
-            val artist: String? = intent.extras?.getString(SONG_ARTIST_KEY, "")
+            val song = intent.extras?.getParcelable<Song>(SONG_KEY)
+
+            val image: Int? = song?.largeImageID
+            val title: String? = song?.title
+            val artist: String? = song?.artist
 
             if (image != null) {
                 ivAlbumCover.setImageResource(image)
             }
             tvSongTitle.text = title
             tvArtists.text = artist
+
+            btnSettings.setOnClickListener {
+                //val defaultSong: Song = SongDataProvider.createRandomSong()
+                //val songToSend = intent.extras?.getParcelable(SONG_KEY)
+                launchSettingsActivity(this@PlayerActivity, song, numPlaysCounter)
+            }
         }
     }
 
--- app/src/main/java/edu/uw/ryanl32/dotify/SettingsActivity.kt
+++ app/src/main/java/edu/uw/ryanl32/dotify/SettingsActivity.kt
@@ -1,11 +1,52 @@
 package edu.uw.ryanl32.dotify
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.ericchee.songdataprovider.Song
+import edu.uw.ryanl32.dotify.databinding.ActivityPlayerBinding
+import edu.uw.ryanl32.dotify.databinding.ActivitySettingsBinding
+//
+//private const val SONG_TITLE_KEY = "song_title"
+//private const val SONG_ARTIST_KEY = "song_artist"
+//private const val SONG_IMAGE_KEY = "song_image"
+private const val SONG_KEY = "song_key"
+private const val PLAYS_KEY = "plays_key"
+
+fun launchSettingsActivity(context: Context, song: Song?, plays: Int) {
+    with(context) {
+        val intent = Intent(context, SettingsActivity::class.java).apply {
+            val bundle = Bundle().apply {
+                putInt(PLAYS_KEY, plays)
+                putParcelable(SONG_KEY, song)
+            }
+            putExtras(bundle)
+        }
+        startActivity(intent)
+    }
+}
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingsBinding
+    private val navController: NavController by lazy { findNavController(R.id.navHost) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+
+        binding = ActivitySettingsBinding.inflate(layoutInflater).apply { setContentView(root) }
+        with(binding) {
+
+            navController.setGraph(R.navigation.nav_graph, intent.extras)
+//            val song = intent.extras?.getParcelable<Song>(SONG_KEY)
+//            val plays = intent.extras?.getInt(PLAYS_KEY)
+//            if (song != null) {
+//                Toast.makeText(this@SettingsActivity, "$song \n PLAYS $plays", Toast.LENGTH_SHORT).show()
+//            }
+        }
     }
 }
